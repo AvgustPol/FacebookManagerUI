55fce460fa0c30e7e84229fbdb2aa623a32d1627
mazariniz <telonily@gmail.com>
2019-03-03 00:26:57 +0100

    UI changes + implemented functions based on FBManager

--- FacebookManagerUI/MainWindow.xaml.cs
+++ FacebookManagerUI/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Threading.Tasks;
+using System.Windows;
+using FBManager;
+
 
 namespace FacebookManagerUI
 {
@@ -7,14 +10,30 @@ namespace FacebookManagerUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private FacebookService _facebookService;
         public MainWindow()
         {
             InitializeComponent();
+            _facebookService = new FacebookService();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            _facebookService.AccessToken = TextBoxAccessToken.Text;
+        }
 
+        private void SetAnswer(string answer)
+        {
+            TextBlockAnswer.Text = answer;
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ButtonGetName.IsEnabled = false;
+            var task = _facebookService.GetUserName();
+            await task;
+            SetAnswer(task.Result);
+            ButtonGetName.IsEnabled = true;
         }
     }
 }
