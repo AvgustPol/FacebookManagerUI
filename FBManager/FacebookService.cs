20e356c8555c4d4a2337d535fcf651d9e6cff5dd
mazariniz <telonily@gmail.com>
2019-03-03 00:25:16 +0100

    Created new project FBManager
    Created new library project, based on .NET Standard
    Implemented FacebookClient and partially FacebookService

---
+++ FBManager/FacebookService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FBManager
+{
+    public class FacebookService
+    {
+        private readonly FacebookClient _facebookClient = new FacebookClient();
+        public string AccessToken { get; set; }
+
+        public async Task<string> GetUserName()
+        {
+            Debug.WriteLine("Entering GetUserName function");
+            if (AccessToken == null)
+                throw new Exception("Access Token is not set");
+
+            var result = await _facebookClient.GetAsync<dynamic>(
+                AccessToken, "me", "fields=name");
+            
+
+            string res = result.name == null ? "" : result.name;
+            return res;
+        }
+    }
+}
